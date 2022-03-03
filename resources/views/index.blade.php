ddde88724265a48b19da2fe1eac0e86da79e6b82
imYours14 <farrelvalentino19@gmail.com>
2022-03-04 00:00:49 +0700

    update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,4 @@
 @extends('main')
-
 @section('content')
 <div class="container">
 	<!-- Top box -->
--- routes/web.php
+++ routes/web.php
@@ -17,10 +17,10 @@ Route::get('/', function () {
     return view('index');
 })->name('home');
 
-Route::get('/', function () {
+Route::get('/about', function () {
     return view('about');
 })->name('about');
 
-Route::get('/', function () {
+Route::get('/contact', function () {
     return view('contact');
 })->name('contact');
